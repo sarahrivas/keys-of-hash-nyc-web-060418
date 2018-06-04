4c5eadc9a80923eab858c63089623820f34b8a3f
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-06-04 01:35:05 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,13 @@
 require "pry"
 class Hash
   def keys_of(*arguments)
-    
-    
+    matching_arr = []
+      self.keys.each do |value|
+        if arguments.include?(value)
+          matching_arr << value
+        end  
+      end
+    end
   end
+  matching_arr
 end
